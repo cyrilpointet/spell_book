8234ca4f1ee803cbc4be34e63d58ee78ff18e476
cyrilpointet <pointet.cyril@gmail.com>
2021-03-07 08:09:47 +0100

    add index and summary to spell, wip add classes

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import { vuetify } from "@/theme/vuetify";
 
 Vue.config.productionTip = false;
 
+// import spells from "@/./spells.json";
+// spells.forEach((elem, index) => {
+//   spells[index].summary = "";
+//   console.log(spells[index].id, index);
+// });
+// localStorage.setItem('spells', JSON.stringify(spells));
+
+// import test from "@/test.html";
+// const toto = test.querySelectorAll('tr');
+// toto.forEach(elem => {
+//   console.log(elem);
+// });
+
 new Vue({
   vuetify,
   render: h => h(App)
